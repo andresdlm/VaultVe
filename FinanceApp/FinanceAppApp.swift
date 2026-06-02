38a3cc419dd1e0bbfd465c6030feac0fffa1c400
Andres Linarez <andreslinarez9@gmail.com>
2026-06-01 23:55:58 -0400

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Andres Linarez on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
